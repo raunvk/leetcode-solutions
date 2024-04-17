22f7b4e789d720568c6e7c8c165f977fb964564e
raunvk <raunakd137@gmail.com>
2024-04-17 20:28:02 +0530

    first commit

---
+++ Easy/Palindrome Number/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        if(x<0)
+        {
+            return false;
+        }
+        else
+        {
+            int copy = x;
+            int sum = 0;
+            while(copy>0)
+            {
+                int rem = copy%10;
+                sum = sum*10 + rem;
+                copy /= 10;
+            }
+            if(sum==x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
