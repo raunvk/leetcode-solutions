66d4dcad37d80573ab81d63e4df56d38e6ef5734
Raunak Das <raunakd137@gmail.com>
2024-04-17 20:47:36 +0530

    first commit

--- Easy/Palindrome Number/Solution.java
+++ Easy/Palindrome Number/Solution.java
@@ -1,26 +1,20 @@
 class Solution {
-    public boolean isPalindrome(int x) 
-    {
-        if(x<0)
-        {
+    public boolean isPalindrome(int x) {
+        if(x<0) {
             return false;
         }
-        else
-        {
+        else {
             int copy = x;
             int sum = 0;
-            while(copy>0)
-            {
+            while(copy>0) {
                 int rem = copy%10;
                 sum = sum*10 + rem;
                 copy /= 10;
             }
-            if(sum==x)
-            {
+            if(sum==x) {
                 return true;
             }
-            else
-            {
+            else{
                 return false;
             }
         }
