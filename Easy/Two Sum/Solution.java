a4f5140b68ebff1ae6e13d7bb52f879d6a28e7bc
Raunak Das <raunakd137@gmail.com>
2024-04-17 20:48:12 +0530

    first commit

--- Easy/Two Sum/Solution.java
+++ Easy/Two Sum/Solution.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) 
-    {
+    public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         int sum;
         int n = nums.length;
-        for(int i=0;i<n-1;i++)
-        {
+        for(int i=0;i<n-1;i++) {
             sum = 0;
-            for(int j=i+1;j<n;j++)
-            {
+            for(int j=i+1;j<n;j++) {
                 sum = nums[i]+nums[j];
-                if(sum == target)
-                {
+                if(sum == target) {
                     res[0] = i;
                     res[1] = j;
                     break;
