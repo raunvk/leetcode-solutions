eab23ca853c442e7ddb18b769e143a144d9c74fe
raunvk <raunakd137@gmail.com>
2024-04-18 19:47:11 +0530

    first commit

---
+++ Easy/Most Common Word/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase();
+        List<String> bannedWords = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : banned){
+            bannedWords.add(s);
+        }
+        String[] words = paragraph.toLowerCase().split("\\W+");
+        for(String s : words){
+            if(!bannedWords.contains(s)){
+                map.put(s, map.getOrDefault(s, 0)+1);
+            }
+        }
+        int max = 0;
+        String result = "";
+        System.out.println(map);
+        for(String s : map.keySet()){
+            if(map.get(s)>max){
+                max = map.get(s);
+                result = s;
+            }
+        }
+        return result;
+    }
+}
