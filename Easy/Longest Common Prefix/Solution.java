12f22e5fc89e5019696da5a607ecca77060004f3
Raunak Das <raunakd137@gmail.com>
2024-04-19 10:58:54 +0530

    first commit

--- Easy/Longest Common Prefix/Solution.java
+++ Easy/Longest Common Prefix/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
         int i = 0;
-        while(i < s1.length() && i < s2.length()){
+        while(i<s1.length() && i<s2.length()){
             if(s1.charAt(i) == s2.charAt(i)){
                 i++;
             } 
