74fe5757019599a24d718be0f9a8cf20db00e201
raunvk <raunakd137@gmail.com>
2024-04-24 16:23:10 +0530

    first commit

---
+++ Easy/Valid Palindrome/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0; 
+        int end = s.length()-1;
+        while(start <= end) {
+            char currStart = s.charAt(start);
+            char currEnd = s.charAt(end);
+            if(!Character.isLetterOrDigit(currStart)) {
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(currEnd)) {
+                end--;
+            }
+            else {
+                if(Character.toLowerCase(currStart) != Character.toLowerCase(currEnd)) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
