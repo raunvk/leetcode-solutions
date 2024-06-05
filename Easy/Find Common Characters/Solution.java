30de29fa7d9779640b1bb725ced034d1f829d984
raunvk <raunakd137@gmail.com>
2024-06-05 13:05:48 +0530

    first commit

---
+++ Easy/Find Common Characters/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int n = words.length;
+        List<String> result = new ArrayList<>();
+        int[] minFreq = new int[26];
+        for (char c : words[0].toCharArray()) {
+            minFreq[c - 'a']++;
+        }
+        for (int i=1; i<n; i++) {
+            int[] freq = new int[26];
+            for (char c : words[i].toCharArray()) {
+                freq[c - 'a']++;
+            }
+            for (int j=0; j<26; j++) {
+                minFreq[j] = Math.min(minFreq[j], freq[j]);
+            }
+        }
+        for (int i=0; i<26; i++) {
+            while (minFreq[i]-- > 0) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return result;
+    }
+}
