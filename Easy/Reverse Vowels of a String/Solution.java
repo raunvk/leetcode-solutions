e8240f495ba1b1a90153faf5b89399f189422e1a
raunvk <raunakd137@gmail.com>
2025-10-14 19:46:15 +0530

    first commit

--- Easy/Reverse Vowels of a String/Solution.java
+++ Easy/Reverse Vowels of a String/Solution.java
@@ -1,24 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
-        List<Character> charList = new ArrayList<>();
-        for(char c : s.toCharArray())
-            if(isVowel(c)) {
-                charList.add(c);
+        char[] arr = s.toCharArray();
+        // Using two-pointers
+        int start = 0, end = s.length()-1;
+        while(start < end) {
+            while(start<end && !isVowel(arr[start]))
+                start++;
+            while(start<end && !isVowel(arr[end]))
+                end--;
+            if(start < end) {
+                swap(arr, start++, end--);
+            }
         }
-        int index = 0;
-        StringBuilder result = new StringBuilder();
-        System.out.println(charList);
-        for(char c : s.toCharArray()) {
-            if(isVowel(c))
-                result.append(charList.get(charList.size()-1-index++));
-            else
-                result.append(c);
-        }
-        return result.toString();
+        return new String(arr);
     }
     private boolean isVowel(char c) {
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
             return true;
         return false;
+    }
+    private void swap(char[] arr, int start, int end) {
+        char temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
