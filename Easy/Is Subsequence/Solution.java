377edc1b9a343c8fac25d52e8df4dc634f742381
raunvk <raunakd137@gmail.com>
2025-10-24 21:24:40 +0530

    first commit

---
+++ Easy/Is Subsequence/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()==0)
+            return true;
+        if(t.length()==0)
+            return false;
+        int index = 0;
+        for(int i=0; i<t.length(); i++) {
+            if(t.charAt(i) == s.charAt(index))
+                index++;
+            if(index == s.length())
+                return true;
+        }
+        return false;
+    }
+}
