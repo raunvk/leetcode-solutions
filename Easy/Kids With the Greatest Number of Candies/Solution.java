a441609a0a3246f07f3ac6b67ca122b0ee6b6b35
raunvk <raunakd137@gmail.com>
2025-11-05 19:59:22 +0530

    first commit

--- Easy/Kids With the Greatest Number of Candies/Solution.java
+++ Easy/Kids With the Greatest Number of Candies/Solution.java
@@ -1,12 +1,9 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = Arrays.stream(candies).max().getAsInt();
-        List<Boolean> result = new ArrayList<>();
-        for(int n : candies) {
-            if(n+extraCandies >= max)
-                result.add(true);
-            else
-                result.add(false);
+        List<Boolean> result = new ArrayList<>(candies.length);
+        for(int c : candies) {
+            result.add(c+extraCandies >= max);
         }
         return result;
     }
